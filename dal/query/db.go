ae1bcd82af3d65c6a514de946f88cfb29408bc7d
chuxin <chuxin0816@qq.com>
2023-12-13 23:19:06 +0800

    增加redis客户端

--- dal/query/db.go
+++ dal/query/db.go
@@ -3,23 +3,38 @@ package query
 import (
 	"fmt"
 
+	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-const dsn = "root:123456@tcp(127.0.0.1:3306)/shorturl?charset=utf8mb4&parseTime=True&loc=Asia%2FShanghai"
+const (
+	dsn       = "root:123456@tcp(127.0.0.1:3306)/shorturl?charset=utf8mb4&parseTime=True&loc=Asia%2FShanghai"
+	redisAddr = "127.0.0.1:6379"
+)
 
-var db *gorm.DB
+var (
+	db  *gorm.DB
+	rdb *redis.Client
+)
 
 func init() {
-	db = connectDB(dsn)
+	connectDB(dsn)
+	connectRedis(redisAddr)
 	SetDefault(db)
 }
 
-func connectDB(dsn string) *gorm.DB {
-	db, err := gorm.Open(mysql.Open(dsn))
+func connectDB(dsn string) {
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn))
 	if err != nil {
 		panic(fmt.Errorf("connect db fail: %w", err))
 	}
-	return db
+}
+
+func connectRedis(addr string) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr: addr,
+		DB:   0,
+	})
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.5
 
 require (
 	github.com/go-playground/validator/v10 v10.16.0
+	github.com/redis/go-redis/v9 v9.3.0
 	github.com/smartystreets/goconvey v1.8.1
 	github.com/zeromicro/go-zero v1.6.0
 	gorm.io/driver/mysql v1.5.2
@@ -16,6 +17,7 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
