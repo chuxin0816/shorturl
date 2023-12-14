f99ad8982397c66f6869728c00ea7bd29a95580a
chuxin <chuxin0816@qq.com>
2023-12-14 22:59:21 +0800

    参数校验

--- internal/handler/showhandler.go
+++ internal/handler/showhandler.go
@@ -7,6 +7,7 @@ import (
 	"shorturl/internal/svc"
 	"shorturl/internal/types"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
@@ -14,6 +15,11 @@ func showHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.ShowRequest
 		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		if err := validator.New().StructCtx(r.Context(), &req); err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 			return
 		}
