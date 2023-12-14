7cd748720e7eecbd4b69abb6a4ba317355881b98
chuxin <chuxin0816@qq.com>
2023-12-14 21:43:43 +0800

    优化分页查询逻辑

--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -72,7 +72,7 @@ func loadDataToBloom() {
 	if err != nil {
 		panic(fmt.Errorf("query.ShortURLMap.WithContext(context.Background()).Count() error: %v", err))
 	}
-	
+
 	count := int(cnt)
 	pageTotal := 0
 	pageSize := 50
@@ -82,12 +82,12 @@ func loadDataToBloom() {
 		pageTotal = count/pageSize + 1
 	}
 
-	for page := 1; page <= pageTotal; page++ {
-		su, err := query.ShortURLMap.WithContext(context.Background()).Where(query.ShortURLMap.IsDel.Eq(0)).Offset((page - 1) * pageSize).Limit(int(pageSize)).Select(query.ShortURLMap.Surl).Find()
+	for page := 0; page < pageTotal; page++ {
+		su, err := query.ShortURLMap.WithContext(context.Background()).Where(query.ShortURLMap.IsDel.Eq(0)).Offset(page * pageSize).Limit(int(pageSize)).Select(query.ShortURLMap.Surl).Find()
 		if err != nil {
 			panic(fmt.Errorf("query.ShortURLMap.WithContext(context.Background()).Find() error: %v", err))
 		}
-		
+
 		for _, v := range su {
 			bloomFilter.Add([]byte(v.Surl))
 		}
